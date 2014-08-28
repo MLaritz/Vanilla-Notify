2a04ce577bf13919d31e68aa50b718780d795288
Martin Laritz <martin.laritz@gmail.com>
2014-08-27 20:16:30 -0400

    Initial commit.

---
+++ Gruntfile.js
@@ -0,0 +1,86 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    distFolder: 'dist',
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    uglify: {
+      app: {
+        src: 'vanilla-notify.js',
+        dest: '<%= distFolder %>/scripts/vanilla-notify.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: false,
+        unused: false,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: '*.js'
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: '',
+          cssDir: '<%= distFolder %>/styles/',
+          environment: 'production'
+        }
+      }
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['default'],
+        options: {
+          spawn: false
+        }
+      },
+      scripts: {
+        files: '*.js',
+        tasks: ['jshint', 'uglify'],
+        options: {
+          spawn: false
+        }
+      },
+      styles: {
+        files: '*.scss',
+        tasks: 'compass',
+        options: {
+          spawn: false
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'compass', 'uglify']);
+
+};
---
+++ vanilla-notify.js
@@ -0,0 +1,128 @@
+var notify = (function() {
+
+  var info = function(text) {
+    return addNotify('notify-info', text);
+  };
+
+  var success = function(text) {
+    return addNotify('notify-success', text);
+  };
+
+  var error = function(text) {
+    return addNotify('notify-error', text);
+  };
+
+  var warning = function(text) {
+    return addNotify('notify-warning', text);
+  };
+
+  var notify = function(text) {
+    return addNotify('notify-notify', text);
+  };
+
+  var addNotify = function(className, text) {
+    var container = getNotifyContainer();
+
+    var frag = document.createDocumentFragment();
+
+    var item = document.createElement('div');
+    item.classList.add('notify-item');
+    item.classList.add(className);
+    item.innerHTML = text;
+    item.style.opacity = 0;
+
+    frag.appendChild(item);
+
+    container.appendChild(frag);
+
+    var hideNotify = function() {
+      fadeOut(item);
+    };
+
+    fadeIn(item);
+
+    interval = setTimeout(hideNotify, 5000);
+
+    return item;
+  };
+
+  var getNotifyContainer = function() {
+
+    var container = document.querySelector('.notify-container');
+
+    if (container) {
+      return container;
+    }
+
+    return createNotifyContainer();
+  };
+
+  var createNotifyContainer = function() {
+    var frag = document.createDocumentFragment();
+    container = document.createElement('div');
+    container.classList.add('notify-container');
+
+    frag.appendChild(container);
+    document.body.appendChild(frag);
+
+    return container;
+  };
+
+      var animate = function(options) {
+            var start = new Date();
+            var id = setInterval(function() {
+                var timePassed = new Date() - start;
+                var progress = timePassed / options.duration;
+                if (progress > 1) {
+                    progress = 1;
+                }
+                options.progress = progress;
+                var delta = options.delta(progress);
+                options.step(delta);
+                if (progress === 1) {
+                    clearInterval(id);
+                    if (options.complete){
+                      options.complete();
+                    }
+                }
+            }, options.delay || 10);
+        };
+        var fadeOut = function(element) {
+            var to = 1;
+            animate({
+                duration: 2000,
+                delta: function(progress) {
+                    progress = this.progress;
+                    return 0.5 - Math.cos(progress * Math.PI) / 2;
+                },
+                complete: function () {
+                  element.style.display = 'none';
+                  element.outerHTML = '';
+                },
+                step: function(delta) {
+                    element.style.opacity = to - delta;
+                }
+            });
+        };
+        var fadeIn = function(element) {
+            var to = 0;
+            animate({
+                duration: 2000,
+                delta: function(progress) {
+                    progress = this.progress;
+                    return 0.5 - Math.cos(progress * Math.PI) / 2;
+                },
+                step: function(delta) {
+                    element.style.opacity = to + delta;
+                }
+            });
+        };
+
+  return {
+    info: info,
+    success: success,
+    error: error,
+    warning: warning,
+    notify: notify
+  };
+})();
