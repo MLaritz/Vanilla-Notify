003cd5b9897cad48781badf6cd9d291fcbb3f78d
Martin Laritz <martin.laritz@gmail.com>
2014-09-01 20:44:17 -0400

    Added vNotify scripts

---
+++ javascripts/vNotify.js
@@ -0,0 +1,5 @@
+vNotify.info('This is an info notification.', 'Info Notification.');
+vNotify.success('This is a success notification.', 'Success Notification.');
+vNotify.warning('This is a warning notification.', 'Warning Notification.');
+vNotify.error('This is an error notification.', 'Error Notification.');
+vNotify.notify('This is a notify notification.', 'Notify Notification.');
